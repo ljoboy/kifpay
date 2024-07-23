6cae78b8ff7c7a49a1247873d7f4cb7518a9e00f
Dark Angel <jonathanyombo@gmail.com>
2024-07-23 16:14:11 +0200

    Frais model

---
+++ app/Models/Frais.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Frais extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'intitule',
+        'montant',
+        'promotion_id'
+    ];
+
+    protected function casts(): array
+    {
+        return  [
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+            'deleted_at' => 'datetime',
+        ];
+    }
+
+    public function promotion(): BelongsTo
+    {
+        return $this->belongsTo(Promotion::class);
+    }
+
+    public function comptes(): BelongsToMany
+    {
+        return $this->belongsToMany(Compte::class);
+    }
+}
